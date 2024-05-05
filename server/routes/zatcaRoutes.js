8fa92805355478990125640835019293de8b736e
shaik Asif <smd.20sa@gmail.com>
2024-05-06 01:41:51 +0530

    smallchange

--- server/routes/zatcaRoutes.js
+++ server/routes/zatcaRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const zatcaController = require("../controllers/ZatcaController");
+const zatcaController = require("../controllers/zatcaController");
 
 router.post("/submit-form-data", zatcaController.submitFormData);
 
